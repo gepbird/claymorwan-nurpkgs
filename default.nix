96b9365d0634396e9e87d7ec4bf29c7295a8fa15
claymorwan <claymorwan@fembois.dev>
2026-09-05 22:49:24 +0200

    twintaillauncher: init at 2.5.0

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@
 
 { pkgs ? import <nixpkgs> { } }:
 
-{
+rec {
   # The `lib`, `overlays`, `nixosModules`, `homeModules`,
   # `darwinModules` and `flakeModules` names are special
   lib = import ./lib { inherit pkgs; }; # functions
@@ -23,5 +23,7 @@
   shimelinux = pkgs.callPackage ./pkgs/shimelinux { };
   utmt-cli = pkgs.callPackage ./pkgs/utmt-cli { };
   pvpn = pkgs.callPackage ./pkgs/pvpn { };
+  twintailauncher = pkgs.callPackage ./pkgs/twintailauncher/twintailauncher { inherit twintailauncher-unwrapped; };
+  twintailauncher-unwrapped = pkgs.callPackage ./pkgs/twintailauncher/twintailauncher-unwrapped { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
 }
